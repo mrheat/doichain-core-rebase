c64db6bf50f6cf3ab958f735429709ba7addfe22
Nico Krause <nico@le-space.de>
2020-10-16 17:05:35 +0500

    logging contents of second valtype

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -854,8 +854,9 @@ bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& 
                     valtype& vch1 = stacktop(-2);
                     valtype& vch2 = stacktop(-1);
                     std::string s(vch1.begin(), vch1.end());
+                    std::string s2(vch1.begin(), vch1.end());
                     bool fEqual = (vch1 == vch2);
-                    LogPrintf("interpreter.cpp : EvalScript 0.8 OP_EQUAL %s\n",s);
+                    LogPrintf("interpreter.cpp : EvalScript 0.8 OP_EQUAL %s $s\n",s,s2);
                     // OP_NOTEQUAL is disabled because it would be too easy to say
                     // something like n != 1 and have some wiseguy pass in 1 with extra
                     // zero bytes after it (numerically, 0x01 == 0x0001 == 0x000001)
